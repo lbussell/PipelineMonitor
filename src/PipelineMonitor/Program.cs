4bc2ab837185609044911b1fead834f250af8321
Logan Bussell <loganbussell@microsoft.com>
2026-01-08 10:39:54 -0800

    Merge pull request #1 from lbussell/publish-as-dotnet-tool
    Configure PipelineMonitor to publish as a .NET tool

---
+++ src/PipelineMonitor/Program.cs
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: Copyright (c) 2026 Logan Bussell
+// SPDX-License-Identifier: MIT
+
+namespace PipelineMonitor;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("PipelineMonitor");
+    }
+}
