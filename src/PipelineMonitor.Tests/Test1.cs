24f823d556bcb9dc0ac268ad3e25a259b0031380
Logan Bussell <loganbussell@microsoft.com>
2026-01-08 09:28:10 -0800

    Initialize with project template

---
+++ src/PipelineMonitor.Tests/Test1.cs
@@ -0,0 +1,13 @@
+﻿// SPDX-FileCopyrightText: Copyright (c) 2026 Logan Bussell
+// SPDX-License-Identifier: MIT
+
+namespace PipelineMonitor.Tests;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
---
+++ src/PipelineMonitor/Class1.cs
@@ -0,0 +1,8 @@
+﻿// SPDX-FileCopyrightText: Copyright (c) 2026 Logan Bussell
+// SPDX-License-Identifier: MIT
+
+namespace PipelineMonitor;
+
+public class Class1
+{
+}
